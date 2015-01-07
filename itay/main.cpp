cea491e167722387e219e6b332b654b3ca1b302f
itaynoama <itay.noama@gmail.com>
2015-01-07 18:24:20 +0200

    qw

--- itay/main.cpp
+++ itay/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  itay
-//
-//  Created by itay noama on 1/7/15.
-//  Copyright (c) 2015 itay. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
