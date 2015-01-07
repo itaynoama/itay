ce602ceaff4a184d8638c9e9d1bc46a1736d5179
itay noama <itaynoama@itay-noama.local>
2015-01-07 18:19:39 +0200

    Initial Commit

---
+++ itay/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  itay
+//
+//  Created by itay noama on 1/7/15.
+//  Copyright (c) 2015 itay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
