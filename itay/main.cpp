ccd881e718f18f4cb4c723651b58aaa722dcd8f5
itay noama <itaynoama@itay-noama.local>
2015-01-07 18:29:24 +0200

    demo

--- itay/main.cpp
+++ itay/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
 
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
